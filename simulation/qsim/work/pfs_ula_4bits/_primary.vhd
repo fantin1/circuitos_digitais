7928c684caada97e7796b548a6af43e6f364b3b9
p-fan <p-fantin@hotmail.com>
2026-04-20 20:17:08 -0300

    ula4 included overvlow

--- simulation/qsim/work/pfs_ula_4bits/_primary.vhd
+++ simulation/qsim/work/pfs_ula_4bits/_primary.vhd
@@ -2,11 +2,14 @@ library verilog;
 use verilog.vl_types.all;
 entity pfs_ula_4bits is
     port(
-        cout            : out    vl_logic;
+        z               : out    vl_logic;
+        s               : out    vl_logic_vector(3 downto 0);
         x               : in     vl_logic_vector(3 downto 0);
         sel             : in     vl_logic_vector(2 downto 0);
         y               : in     vl_logic_vector(3 downto 0);
-        debug           : out    vl_logic;
-        s               : out    vl_logic_vector(3 downto 0)
+        n               : out    vl_logic;
+        c               : out    vl_logic;
+        b               : out    vl_logic;
+        v               : out    vl_logic
     );
 end pfs_ula_4bits;
--- simulation/qsim/work/pfs_ula_4bits_vlg_check_tst/_primary.vhd
+++ simulation/qsim/work/pfs_ula_4bits_vlg_check_tst/_primary.vhd
@@ -2,9 +2,12 @@ library verilog;
 use verilog.vl_types.all;
 entity pfs_ula_4bits_vlg_check_tst is
     port(
-        cout            : in     vl_logic;
-        debug           : in     vl_logic;
+        b               : in     vl_logic;
+        c               : in     vl_logic;
+        n               : in     vl_logic;
         s               : in     vl_logic_vector(3 downto 0);
+        v               : in     vl_logic;
+        z               : in     vl_logic;
         sampler_rx      : in     vl_logic
     );
 end pfs_ula_4bits_vlg_check_tst;
